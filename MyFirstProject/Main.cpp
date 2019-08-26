76b16db7247754f4c9387fc4f4fea220117a0cc8
Mason204 <Mason204@users.noreply.github.com>
2019-08-25 20:00:44 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,18 @@
+/*
+Mason Henry
+Fall 2019
+Due:August 29 2019
+Lab 1 My First Project
+In this project we are supposed to download a project from Github, compile and run it, and then recreate the project and upload it.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+
+	std::cout << "My favorite colour is orange.";
+
+	return 0;
+}
